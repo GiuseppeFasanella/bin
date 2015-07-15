a195ede88fd0b6693974b0356df57cd3f1c5aaa5
Giuseppe <I am Amerigo, you know my mail>
2015-07-15 22:45:39 +0200

    second commit

--- killer_ear_trainer.sh
+++ killer_ear_trainer.sh
@@ -1,5 +1,2 @@
 #! /bin/bash
-
-kill $(ps ax| grep mplayer | grep Sl+ | cut -f2 -d " ")
-file_to_play=$(head -1 $1list.dat)
-sed -i "/${file_to_play}/d" $1list.dat
+kill $(ps ax | grep "bin/bash /home/gfasanel/bin/ear_trainer.sh" | grep -v "grep" | grep S+ | cut -f2 -d " ")
