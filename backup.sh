979a34ebafb684b90cb1e2f14a22191c250ffb45
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-07-31 23:11:03 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,21 @@
-#! /bin/bash
+#! /bin/bash                                                                                                                                                                     
 cd Scrivania
-tar czf Dipho.tar.gz Dipho
-tar czf ecalelf_run2.tar.gz ecalelf_run2 
-tar czf Zprime.tar.gz Zprime 
-tar czf Latex.tar.gz Latex 
-tar czf latex_progetti.tar.gz latex_progetti;
+##df -aTH #lista i device montati                                                                                                                                                
+mv /media/SanDisk/Scrivania_pc_backup/*.tar.gz /media/SanDisk/Scrivania_pc_backup/temp/
+echo "backup of Dipho"
+tar czf /media/SanDisk/Scrivania_pc_backup/Dipho.tar.gz Dipho
+echo "backup of ecalelf_run2"
+tar czf /media/SanDisk/Scrivania_pc_backup/ecalelf_run2.tar.gz ecalelf_run2
+echo "backup of Zprime"
+tar czf /media/SanDisk/Scrivania_pc_backup/Zprime.tar.gz Zprime
+echo "backup of Latex"
+tar czf /media/SanDisk/Scrivania_pc_backup/Latex.tar.gz Latex
+echo "backup of latex_progetti"
+tar czf /media/SanDisk/Scrivania_pc_backup/latex_progetti.tar.gz latex_progetti;
+
+echo "backup of .bashrc and .emacs"
+cp ~/.bashrc /media/SanDisk/dotFiles/
+cp ~/.emacs /media/SanDisk/dotFiles/
+
+
+
